b4394efaf02bcdc010f0dfa118e613a80f3464bf
Arthur Cantarela <cantarela.arthur@gmail.com>
2020-10-01 06:39:25 -0300

    First commit

---
+++ buffer_circular/src/main.c
@@ -0,0 +1,40 @@
+/* Teste de um buffer circular
+ *
+ * Este programa serve para implementar e testar uma implementacao de buffer
+ * circular
+ */
+
+#include <stdio.h>
+
+char buffer[5];
+
+void adicionar_buffer(char c) {
+/* Preencha com seu codigo! */
+
+}
+
+void remover_buffer() {
+/* Preencha com seu codigo! */
+
+}
+
+void imprimir_buffer() {
+/* Preencha com seu codigo! */
+
+}
+
+int main() {
+
+
+  char c;
+
+  do {
+    scanf("%c", &c);
+    if (c == '\n') break;
+
+  /* Sugestao: aqui deveria vir o seu codigo */
+
+  } while (1);
+
+  return 0;
+}
